a3002c923d1c862f18f079c4b9f3f369ae7b3e4e
DARKFU1 <dark_ever@mail.ru>
2023-05-08 18:26:42 +0300

    Stable version of the program

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define watch_int(x) printf("%s: %i\n", #x, x);
+
+int IsALetter(const char ch)
+{
+	return (ch > 64 && ch < 91) || (ch > 96 && ch < 123);
+}
+
+int main(const int argc, const char** argv)
+{
+	FILE* file;
+
+	if(argc > 1)
+	{
+		file = fopen(argv[1], "a");
+		fprintf(file, "");
+		fclose(file);
+
+		file = fopen(argv[1], "r");
+	}
+	else
+	{
+		file = stdin;
+	}
+
+	int inp;
+	int i = 0; 
+
+	int lines = 0;
+	unsigned int words = 0;
+
+	char buffer[64000];
+
+	while(inp != EOF)
+	{
+		inp = getc(file);
+		buffer[i++] = inp;
+
+		if(inp == '\n')
+		{
+			++lines;
+		}
+
+		if( i < 2 ) continue;
+
+		if((buffer[i-2] != ' ' &&\
+			buffer[i-2] != '\t') &&\
+			(inp == ' ' || inp == '\t'))
+		{
+			++words;
+		}
+
+	}
+
+	printf("\n~~~~~~~~~~~~~~~~~~ FILE CONSISTANCE ~~~~~~~~~~~~~~~~~~\n");
+	printf("Lines: %i\n", lines);
+	printf("Words: %i\n", words + 1);
+	printf("Symbols: %i\n", i - 1);
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+
+	return 0;
+}
